7dd77f0168bbb201bf9c8c9a0bbbcf8da93eaf03
GarotoCowboy <rocha.pedrohenrique91@gmail.com>
2025-04-19 13:06:09 -0300

    feat: created user struct

--- api/models/user.go
+++ api/models/user.go
@@ -1,1 +1,13 @@
 package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Username  string `json:"username"  gorm:"not null;unique"`
+	Password  string `json:"password"    gorm:"not null"`
+	Email     string `json:"email"   gorm:"not null"`
+	Firstname string `json:"firstname"   gorm:"not null"`
+	Lastname  string `json:"lastname"    gorm:"not null"`
+	ImageLink string `json:"imageLink"`
+}
