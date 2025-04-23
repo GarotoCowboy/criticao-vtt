896fc44c07d3ea6eb80377e385a80563f48af00f
Pedro Henrique Marques <rocha.pedrohenrique91@gmail.com>
2025-04-23 18:38:45 -0300

    feat: add getUser handler logic

--- api/models/user.go
+++ api/models/user.go
@@ -8,7 +8,7 @@ type User struct {
 	gorm.Model
 	Username  string `json:"username"  gorm:"not null;unique"`
 	Password  string `json:"password"    gorm:"not null"`
-	Email     string `json:"email"   gorm:"not null"`
+	Email     string `json:"email"   gorm:"not null;unique"`
 	Firstname string `json:"firstname"   gorm:"not null"`
 	Lastname  string `json:"lastname"    gorm:"not null"`
 	ImageLink string `json:"imageLink"`
