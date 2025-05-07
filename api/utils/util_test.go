5899b6cf3497d61808ccc8f944c61eb404231e70
Pedro Henrique Marques <rocha.pedrohenrique91@gmail.com>
2025-05-07 09:59:58 -0300

    feat: add util test class and added tests with utils

---
+++ api/utils/util_test.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGenerateSucess(t *testing.T) {
+	size := 10
+
+	result := StringWithCharset(size, charset)
+
+	if len(result) != size {
+		t.Errorf("generate result length %d != %d", len(result), size)
+	}
+	fmt.Println(result)
+}
+
+func TestGenerateWrongSize(t *testing.T) {
+	expectedSize := 10
+	actualSize := 9
+
+	result := StringWithCharset(actualSize, charset)
+
+	if len(result) != expectedSize {
+		// esse teste "falha de propósito" para mostrar erro
+		t.Errorf("Generated result is (%d) it's different to (%d)", len(result), expectedSize)
+	} else {
+		t.Logf("expected flaws, but the sizes matched")
+	}
+}
