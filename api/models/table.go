ff07a119e2863cc56132922e28a7735a06cfd793
Pedro Henrique Marques <rocha.pedrohenrique91@gmail.com>
2025-05-07 10:00:30 -0300

    feat: added table model

--- api/models/table.go
+++ api/models/table.go
@@ -2,15 +2,18 @@ package models
 
 import "gorm.io/gorm"
 
-
-//todo: TA ERRADO TEM Q ARRUMAR >:(
-type Table struct{
+// todo: TA ERRADO TEM Q ARRUMAR >:(
+type Table struct {
 	gorm.Model
+
 	Name string `json:"name" gorm:"not null"`
-	PlayerList []User
-	GameMasters []User
-	InviteLink string
 
+	OwnerID uint `json:"owner_id"`
+	Owner   User `json:"owner" gorm:"foreignKey:OwnerID"`
 
+	Members []TableUser `gorm:"foreignKey:TableID"`
 
+	InviteLink string `json:"inviteLink" gorm:"unique"`
+	Password   string `json:"password"`
+	//ActionLog []string `json:"actionLog"`
 }
