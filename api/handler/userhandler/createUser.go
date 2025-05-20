2a50a5c40f4851c22e494469893876ed459e2c66
Pedro Henrique Marques <rocha.pedrohenrique91@gmail.com>
2025-05-19 21:14:23 -0300

    feat: created the service package and separation of responsibilities between handler and service

--- api/handler/userhandler/createUser.go
+++ api/handler/userhandler/createUser.go
@@ -1,10 +1,9 @@
 package userhandler
 
 import (
-	"github.com/GarotoCowboy/vttProject/api/dto"
+	"github.com/GarotoCowboy/vttProject/api/dto/userDTO"
 	"github.com/GarotoCowboy/vttProject/api/handler"
-	"github.com/GarotoCowboy/vttProject/api/models"
-	"github.com/GarotoCowboy/vttProject/api/utils"
+	userService "github.com/GarotoCowboy/vttProject/api/service/user"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -18,56 +17,26 @@ import (
 // @Tags User
 // @Accept json
 // @Produce json
-// @Param user body UpdateUserRequest true "User data"
-// @Success 200 {object} dto.UserResponse "User Created sucessfully"
-// @Failure 400 {object} dto.ErrorResponse "Bad request error"
-// @Failure 500 {object} dto.ErrorResponse "Internal Server Error"
+// @Param user body userDTO.CreateUserRequest true "User data"
+// @Success 200 {object} userDTO.UserResponse "User Created sucessfully"
+// @Failure 400 {object} userDTO.ErrorResponse "Bad request error"
+// @Failure 500 {object} userDTO.ErrorResponse "Internal Server Error"
 // @Router /user [post]
 func CreateUserHandler(ctx *gin.Context) {
-	request := dto.CreateUserRequest{}
-	//err := ctx.BindJSON(&request)
+	request := userDTO.CreateUserRequest{}
+
 	if err := ctx.BindJSON(&request); err != nil {
 		handler.GetHandlerLogger().ErrorF("Error binding json: %v", err.Error())
 		handler.SendError(ctx, http.StatusBadRequest, "invalid request body")
 		return
 	}
 
-	if err := request.Validate(); err != nil {
-		handler.GetHandlerLogger().ErrorF("validation error: %v", err.Error())
-		handler.SendError(ctx, http.StatusBadRequest, err.Error())
-		return
-	}
-
-	user := models.User{
-		Firstname: request.Firstname,
-		Lastname:  request.Lastname,
-		Email:     request.Email,
-		ImageLink: request.ImageLink,
-		Password:  request.Password,
-		Username:  request.Username,
-	}
-
-	//hash user password
-	hashedPassword, err := utils.HashPassword(user.Password)
+	user, err := userService.CreateUser(handler.GetHandlerDB(), request)
 	if err != nil {
-		handler.GetHandlerLogger().ErrorF("Error hashing password: %v", err.Error())
-		handler.SendError(ctx, http.StatusBadRequest, err.Error())
-		return
-	}
-	user.Password = string(hashedPassword)
-
-	// generic validation email
-	if err := utils.ValidadeEmail(user.Email); err != nil {
-		handler.GetHandlerLogger().ErrorF("Error verifying email: %v", err)
-		handler.SendError(ctx, http.StatusBadRequest, err.Error())
-		return
-	}
-
-	if err := handler.GetHandlerDB().Create(&user).Error; err != nil {
 		handler.GetHandlerLogger().ErrorF("Error creating user: %v", err.Error())
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		handler.SendError(ctx, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	handler.SendSucess(ctx, "create-user", user)
+	handler.SendSucess(ctx, "create-userDTO", user)
 }
