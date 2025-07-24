36d73183b18e16ac3642aaaa0edc0a1459dddf0c
Pedro Henrique <rocha.pedrohenrique91@gmail.com>
2025-07-24 11:07:59 -0300

    refactor: refactored main method to implement grpc server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/GarotoCowboy/vttProject/api/grpc/server"
 	"github.com/GarotoCowboy/vttProject/api/router"
 	"github.com/GarotoCowboy/vttProject/config"
 )
@@ -9,6 +10,15 @@ var (
 	logger *config.Logger
 )
 
+// @title VTT API
+// @version 1.0.0
+// @description The VTT API for managing a VTT.
+// @contact.name Pedro Henrique Marques
+// @contact.email comercial.pedromarques@gmail.com
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+// @host localhost:8080
+// @BasePath /api/v1
 func main() {
 
 	logger = config.GetLogger("main")
@@ -19,13 +29,22 @@ func main() {
 		logger.ErrorF("config initialization error: %v", err)
 		return
 	}
-
+	//create img folder if not exits
 	if err := config.CreateImgFolder(); err != nil {
-		logger.ErrorF("Error creating image folder: %v", err)
+		logger.ErrorF("Error... Creating image folder: %v", err)
 		return
 	}
 
+	//create a file folder if not exists example pdf
+	if err := config.CreateFileFolder(); err != nil {
+		logger.ErrorF("Error... Creating file folder: %v", err)
+		return
+	}
+
+	db := config.GetPostgreSQL()
+
 	//Initialize the server
+	go server.RunGRPCServer(db, logger)
 	router.Initializer()
 
 }
