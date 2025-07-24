752043ea25a50bacca8adb788bcc1f5d6f2121da
Pedro Henrique <rocha.pedrohenrique91@gmail.com>
2025-07-24 11:22:16 -0300

    refactor: refactored table

--- api/models/table.go
+++ api/models/table.go
@@ -2,7 +2,7 @@ package models
 
 import "gorm.io/gorm"
 
-// todo: TA ERRADO TEM Q ARRUMAR >:(
+// create an table
 type Table struct {
 	gorm.Model
 
@@ -11,7 +11,7 @@ type Table struct {
 	OwnerID uint `json:"owner_id"`
 	Owner   User `json:"owner" gorm:"foreignKey:OwnerID;constraint:OnUpdate:CASCADE;constraint:OnDelete:CASCADE"`
 
-	Members []TableUser `gorm:"foreignKey:TableID"`
+	Members []TableUser `gorm:"foreignKey:TableID;constraint:OnDelete:CASCADE"`
 
 	InviteLink string `json:"inviteLink" gorm:"unique"`
 	Password   string `json:"password"`
