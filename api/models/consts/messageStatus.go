963b669b72c6ff2ff42f52ee39220c6f62770289
Pedro Henrique <rocha.pedrohenrique91@gmail.com>
2025-07-24 11:04:04 -0300

    feat: enums to chat

---
+++ api/models/consts/messageStatus.go
@@ -0,0 +1,9 @@
+package consts
+
+type MessageStatus uint8
+
+const (
+	SEND      = 0
+	DELIVERED = 1
+	READ      = 2
+)
---
+++ api/models/consts/messageType.go
@@ -0,0 +1,11 @@
+package consts
+
+type MessageType uint8
+
+const (
+	TEXT   = 0
+	IMAGE  = 1
+	VIDEO  = 2
+	AUDIO  = 3
+	SYSTEM = 4
+)
