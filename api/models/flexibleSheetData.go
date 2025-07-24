ab8f6143f26ab9d3ff489e6a68374f105f333d8b
Pedro Henrique <rocha.pedrohenrique91@gmail.com>
2025-07-24 11:10:58 -0300

    feat: created provised sheet and service to Tormenta 20 rpg System

---
+++ api/models/flexibleSheetData.go
@@ -0,0 +1,20 @@
+package models
+
+//type FlexibleSheetData struct {
+//	Attributes map[string]interface{} `json:"attributes,omitempty"`
+//	Skills map[string]interface{} `json:"skills,omitempty"`
+//	Equipment []map[string]interface{} `json:"equipment,omitempty"`
+//	Abilities []map[string]interface{} `json:"abilities,omitempty"`
+//	Armor map[string]interface{} `json:"armor,omitempty"`
+//	Attacks []map[string]interface{} `json:"attacks,omitempty"`
+//	Hp map[string]interface{} `json:"hp,omitempty"`
+//	Mana map[string]interface{} `json:"mana,omitempty"`
+//
+//	Race string `json:"race,omitempty"`
+//	Origin string `json:"origin,omitempty"`
+//	Deity string `json:"deity,omitempty"`
+//	ClassAndLevel map[string]interface{} `json:"class_and_level,omitempty"`
+//
+//	SystemSpecifies map[string]interface{} `json:"system_specifies,omitempty"`
+//	Notes string `json:"notes,omitempty"`
+//}
