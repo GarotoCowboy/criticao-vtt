d96858f2b8bf0c5567fd10f1b1e1015d7c2e5d69
Pedro Henrique <rocha.pedrohenrique91@gmail.com>
2025-08-28 14:35:46 -0300

    feat: implemented token model

---
+++ api/models/token.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Token struct {
+	gorm.Model
+	Name     string `json:"name"`
+	ImageURL string `json:"image_url"`
+	Bars     []*Bar `json:"bars" gorm:"foreignkey:TokenID"`
+
+	TableID uint  `json:"table_id" gorm:"not null"`
+	Table   Table `json:"table" gorm:"foreignkey:TableID"`
+}
