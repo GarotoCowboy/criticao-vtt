ffe59085f1792777be8f5cdebf25a97993e44596
Pedro Henrique <rocha.pedrohenrique91@gmail.com>
2025-09-24 08:43:03 -0300

    feat:Created model to bar

--- api/models/bar.go
+++ api/models/bar.go
@@ -4,11 +4,10 @@ import "gorm.io/gorm"
 
 type Bar struct {
 	gorm.Model
-	Name     string `json:"name"`
-	Value    int    `json:"value"`
-	MaxValue int    `json:"max_value"`
+	Name     string `json:"name" gorm:"not null"`
+	Value    int32  `json:"value"`
+	MaxValue int32  `json:"max_value"`
 	Color    string `json:"color"`
-
-	TokenID uint  `json:"token_id" gorm:"not null"`
-	Token   Token `json:"token" gorm:"foreignKey:TokenID"`
+	TokenID  uint   `json:"token_id" gorm:"not null"`
+	Token    Token  `json:"token" gorm:"foreignKey:TokenID"`
 }
