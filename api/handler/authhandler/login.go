89e08906a552b35bc6e33265b113f5f7d3b7dc85
Pedro Henrique <rocha.pedrohenrique91@gmail.com>
2025-10-17 08:24:14 -0300

    feat(auth): Implements user ID extraction via token
    Implements an authentication mechanism to obtain the authenticated user's ID directly from the token (e.g., JWT) for use in REST API functions.
    
    - Endpoints that previously received the user ID as a route parameter or request body have been modified.
    - Authentication middleware is now responsible for validating the token and injecting user information into the request context.
    - Service functions have been updated to consume the user ID from this context, centralizing security logic and increasing operational integrity.

---
+++ api/handler/authhandler/login.go
@@ -0,0 +1,29 @@
+package authhandler
+
+import (
+	"net/http"
+
+	"github.com/GarotoCowboy/vttProject/api/handler"
+	"github.com/GarotoCowboy/vttProject/api/service/auth"
+	"github.com/gin-gonic/gin"
+)
+
+func LoginHandler(ctx *gin.Context) {
+	var request auth.LoginRequest
+
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		handler.SendError(ctx, http.StatusBadRequest, "invalid requisition body, fields 'username' and 'password' is necessary")
+		return
+	}
+
+	token, err := auth.LoginService(handler.GetHandlerDB(), request)
+	if err != nil {
+		handler.SendError(ctx, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "login successful",
+		"token":   token,
+	})
+}
